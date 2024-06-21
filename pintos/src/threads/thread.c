6597850af75588b0e7e87fa71edfa490922b5d1f
Hepheir <hepheir@gmail.com>
2024-06-22 05:35:57 +0900

    feat(threads/thread): use `list_insert_ordered()` instead of `list_push_back()` to perform priority scheduling

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -246,7 +246,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem, thread_priority_sort_func, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -317,7 +317,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread)
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, thread_priority_sort_func, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
